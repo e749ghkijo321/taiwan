90ad83986c065be13a88f44d789e7001d5dd949c
e749ghkijo321 <47841892+e749ghkijo321@users.noreply.github.com>
2020-06-01 23:34:25 +0800

    Add files via upload

--- js/box.js
+++ js/box.js
@@ -1,4 +1,6 @@
- var instanc = $(".box");
+ var instanc = $(".box"),
+     clear = $("#clear");
+
  $.each(instanc, function(key, value) {
 
      var pic1 = $(instanc[key]).find("#firstpic"),
@@ -6,7 +8,6 @@
          pic3 = $(instanc[key]).find("#close"),
          show = $(instanc[key]).find("#show"),
          boxx = $(instanc[key]);
-
 
      $(pic1).on('click', function() {
 
@@ -33,5 +34,15 @@
 
      });
 
+
+
+
+ });
+
+ $(clear).on('click', function() {
+
+     instanc.addClass("col-md-3 col-sm-4 col-6 my-3");
+     instanc.find("#show").addClass("d-none");
+     instanc.find("#firstpic").removeClass("d-none");
 
  });
